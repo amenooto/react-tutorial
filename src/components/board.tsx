44cda5366bd86aeb06ba30ae313d7419740b7af5
amenooto_ <amenooto_@naver.com>
2022-12-28 19:32:59 +0900

    fix. 승자 지정

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -5,7 +5,14 @@ import square from "./square";
 const Board = () => {
     const [squares, setSquares] = useState(Array(9).fill(null))
     const [xIsNext, setXisNext] = useState(true)
-    const status = `Next Player: ${xIsNext ? 'X' : 'O'}`
+
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
 
     const handleClick = (val:number) => {
         const item = squares.slice();
@@ -15,6 +22,26 @@ const Board = () => {
     }
     const renderSquare = (val:number) => {
         return <Square result={squares[val]} value={val} event={handleClick}/>
+    }
+
+    function calculateWinner(squares:any) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
     }
 
     return (
